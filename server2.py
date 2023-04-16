e8690c78947bbb5618b6c77f108f44f13eff8a2e
alaeddineallouchi <129352762+alaeddineallouchi@users.noreply.github.com>
2023-04-16 23:51:42 +0100

    create new endpoin to support temperature

--- server2.py
+++ server2.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def welcome() : 
     return"Hello_World"
 
+
+@app.route("/temp")
+def welcome() : 
+    return"temp is 20 deg"
+
 if (__name__=="__main__") : 
     app.run()
